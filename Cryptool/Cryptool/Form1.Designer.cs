a877ec26abfde3929fa2cd3f3632f4fadb0ed2b0
Toanttt <22521490@gm.uit.edu.vn>
2024-12-19 22:47:40 +0700

    khoi tao

---
+++ Cryptool/Cryptool/Form1.Designer.cs
@@ -0,0 +1,440 @@
+﻿namespace Cryptool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button1 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            richTextBox2 = new RichTextBox();
+            label2 = new Label();
+            panel2 = new Panel();
+            richTextBox1 = new RichTextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            richTextBox3 = new RichTextBox();
+            richTextBox4 = new RichTextBox();
+            label6 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label7 = new Label();
+            panel3 = new Panel();
+            label10 = new Label();
+            richTextBox8 = new RichTextBox();
+            label11 = new Label();
+            richTextBox9 = new RichTextBox();
+            label12 = new Label();
+            richTextBox6 = new RichTextBox();
+            panel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(-1, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1043, 45);
+            panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(76, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Cryptool";
+            label1.Click += label1_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(769, 3);
+            button6.Name = "button6";
+            button6.Size = new Size(64, 42);
+            button6.TabIndex = 6;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(699, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(64, 42);
+            button5.TabIndex = 6;
+            button5.Text = "Save";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(629, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(64, 42);
+            button4.TabIndex = 6;
+            button4.Text = "Open";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(559, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(64, 42);
+            button1.TabIndex = 0;
+            button1.Text = "New";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(66, 62);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(943, 549);
+            tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(richTextBox9);
+            tabPage1.Controls.Add(label12);
+            tabPage1.Controls.Add(button8);
+            tabPage1.Controls.Add(button9);
+            tabPage1.Controls.Add(label7);
+            tabPage1.Controls.Add(panel3);
+            tabPage1.Controls.Add(richTextBox8);
+            tabPage1.Controls.Add(label11);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(935, 516);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button7);
+            tabPage2.Controls.Add(button3);
+            tabPage2.Controls.Add(richTextBox1);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(panel2);
+            tabPage2.Controls.Add(richTextBox2);
+            tabPage2.Controls.Add(label2);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(935, 516);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            tabPage2.Click += tabPage2_Click;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(63, 73);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(396, 150);
+            richTextBox2.TabIndex = 10;
+            richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(63, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Cryptool";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(richTextBox4);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(richTextBox3);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(63, 243);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(821, 184);
+            panel2.TabIndex = 11;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(488, 73);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(396, 150);
+            richTextBox1.TabIndex = 13;
+            richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(488, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 20);
+            label3.TabIndex = 12;
+            label3.Text = "Cryptool";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(28, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Cryptool";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(28, 36);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Cryptool";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(28, 72);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(368, 39);
+            richTextBox3.TabIndex = 14;
+            richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(425, 72);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(368, 39);
+            richTextBox4.TabIndex = 16;
+            richTextBox4.Text = "";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(425, 36);
+            label6.Name = "label6";
+            label6.Size = new Size(66, 20);
+            label6.TabIndex = 15;
+            label6.Text = "Cryptool";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(28, 129);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 14;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(63, 456);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 15;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(163, 456);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 16;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(163, 456);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 29);
+            button8.TabIndex = 23;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(63, 456);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 29);
+            button9.TabIndex = 22;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(488, 28);
+            label7.Name = "label7";
+            label7.Size = new Size(51, 20);
+            label7.TabIndex = 20;
+            label7.Text = "Matrix";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(richTextBox6);
+            panel3.Controls.Add(label10);
+            panel3.Location = new Point(63, 243);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(821, 184);
+            panel3.TabIndex = 19;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(28, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(49, 20);
+            label10.TabIndex = 8;
+            label10.Text = "Result";
+            // 
+            // richTextBox8
+            // 
+            richTextBox8.Location = new Point(63, 73);
+            richTextBox8.Name = "richTextBox8";
+            richTextBox8.Size = new Size(369, 32);
+            richTextBox8.TabIndex = 18;
+            richTextBox8.Text = "";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(63, 28);
+            label11.Name = "label11";
+            label11.Size = new Size(80, 20);
+            label11.TabIndex = 17;
+            label11.Text = "Cipher Key";
+            // 
+            // richTextBox9
+            // 
+            richTextBox9.Location = new Point(63, 153);
+            richTextBox9.Name = "richTextBox9";
+            richTextBox9.Size = new Size(369, 70);
+            richTextBox9.TabIndex = 25;
+            richTextBox9.Text = "";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(63, 108);
+            label12.Name = "label12";
+            label12.Size = new Size(74, 20);
+            label12.TabIndex = 24;
+            label12.Text = "Input Text";
+            // 
+            // richTextBox6
+            // 
+            richTextBox6.Location = new Point(19, 35);
+            richTextBox6.Name = "richTextBox6";
+            richTextBox6.Size = new Size(782, 138);
+            richTextBox6.TabIndex = 22;
+            richTextBox6.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(1040, 651);
+            Controls.Add(tabControl1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "EncryptTool";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private Button button1;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Label label1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private RichTextBox richTextBox2;
+        private Label label2;
+        private RichTextBox richTextBox1;
+        private Label label3;
+        private Panel panel2;
+        private Button button2;
+        private RichTextBox richTextBox4;
+        private Label label6;
+        private RichTextBox richTextBox3;
+        private Label label5;
+        private Label label4;
+        private Button button7;
+        private Button button3;
+        private Button button8;
+        private Button button9;
+        private Label label7;
+        private Panel panel3;
+        private Label label10;
+        private RichTextBox richTextBox8;
+        private Label label11;
+        private RichTextBox richTextBox9;
+        private Label label12;
+        private RichTextBox richTextBox6;
+    }
+}
---
+++ Cryptool/Cryptool/Form1.cs
@@ -0,0 +1,35 @@
+namespace Cryptool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
